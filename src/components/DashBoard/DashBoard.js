f8d645375829878620a545b967fd483e2dabcfb4
myishatunsamiha <myisha.samiha@gmail.com>
2022-04-05 15:46:34 +0600

    Added line chart, area chart, bar chart and pie chart on dashboard page.

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -1,11 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
 import './DashBoard.css';
 
 const DashBoard = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setData(data))
+    }, []);
+
     return (
-        <div>
-            Dashboard
+        <div className='dashboard-container'>
+            <div className='dashboard'>
+                <h3>MONTH VS SELL</h3>
+                <LineChart width={400} height={400} data={data}>
+                    <XAxis dataKey='month' />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey='sell' stroke="#8884d8"></Line>
+                </LineChart>
+            </div>
+
+            <div className='dashboard'>
+                <h3>INVESTMENT VS REVENUE</h3>
+                <AreaChart width={420} height={380} data={data}
+                    margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                    <defs>
+                        <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                        </linearGradient>
+                        <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                        </linearGradient>
+                    </defs>
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Area type="monotone" dataKey="investment" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+                    <Area type="monotone" dataKey="revenue" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+                </AreaChart>
+            </div>
+
+            <div className="dashboard">
+                <h3>INVESTMENT VS REVENUE</h3>
+                <BarChart width={400} height={400} data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey='month' />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="investment" fill="#8884d8" />
+                    <Bar dataKey="revenue" fill="#82ca9d" />
+                </BarChart>
+            </div>
+
+            <div className="dashboard">
+                <h3>INVESTMENT VS REVENUE</h3>
+                <PieChart width={400} height={400} data={data}>
+                    <Pie data={data} dataKey="investment" nameKey="month" cx="50%" cy="50%" outerRadius={80} fill="#8884d8" />
+                    <Pie data={data} dataKey="revenue" nameKey="month" cx="50%" cy="50%" innerRadius={90} outerRadius={110} fill="#82ca9d" label />
+                    <Tooltip />
+                </PieChart>
+            </div>
         </div>
+
+
     );
 };
 
